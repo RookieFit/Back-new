9723989df02326d1297fbbdcfd6c93a95986cfa9
mjKim <106668821+pipakmj@users.noreply.github.com>
2025-02-14 00:10:28 +0900

    feat: jwt security 설정 및 테스트

--- back/src/main/kotlin/com/rookiefit/rookiefit/config/SecurityConfig.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/config/SecurityConfig.kt
@@ -1,29 +1,29 @@
 package com.rookiefit.rookiefit.config
 
+import com.rookiefit.rookiefit.filter.JwtAuthenticationFilter
+import com.rookiefit.rookiefit.provider.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
-    @Bean
-    fun passwordEncoder() : BCryptPasswordEncoder{
-        return BCryptPasswordEncoder()
-    }
-
+@EnableWebSecurity
+class SecurityConfig(private val jwtProvider: JwtProvider) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeHttpRequests() { auth ->
+            .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/api/auth/**", "/api/**").permitAll()
                     .anyRequest().authenticated()
             }
             .csrf { csrf ->
                 csrf.disable() //테스트단계에서 비활성화
             }
+            .addFilterBefore(JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
 }
---
+++ back/src/main/kotlin/com/rookiefit/rookiefit/provider/JwtProvider.kt
@@ -0,0 +1,80 @@
+package com.rookiefit.rookiefit.provider
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.JwtParser
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.security.Key
+import java.util.Date
+import javax.crypto.spec.SecretKeySpec
+
+@Component
+class JwtProvider(@Value("\${secretKey}") private val secretKey: String) {
+    private val expirationTime: Long = 3600000
+
+    //접근 토큰 생성
+    fun generateAccessToken(userId: String) : String {
+        val key = SecretKeySpec(secretKey.toByteArray(), SignatureAlgorithm.HS256.jcaName)
+        return Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + expirationTime))
+            .signWith(key)
+            .compact()
+    }
+
+    //리프레시 토큰 생성
+    fun generateRefreshToken(userId: String) : String {
+        val key = SecretKeySpec(secretKey.toByteArray(), SignatureAlgorithm.HS256.jcaName)
+        return Jwts.builder()
+            .setSubject(userId)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + 604800000)) // 7주일
+            .signWith(key)
+            .compact()
+    }
+
+    //토큰에서 username 추출
+    fun extractUserId(token: String): String? {
+        return getClaims(token).subject
+    }
+
+    //토큰 유효성 검사
+    fun validateToken(token: String): Boolean {
+        return  try {
+            getClaims(token)
+            true
+        }catch (e: Exception) {
+            when (e) {
+                is io.jsonwebtoken.ExpiredJwtException -> {
+                    println("만료된 토큰: ${e.message}")
+                    false
+                }
+                is io.jsonwebtoken.UnsupportedJwtException -> {
+                    println("형식과 맞지않는 토큰: ${e.message}")
+                    false
+                }
+                is io.jsonwebtoken.ClaimJwtException -> {
+                    println("JWT 권한 claim 검사 실패: ${e.message}")
+                    false
+                }
+                is io.jsonwebtoken.MalformedJwtException -> {
+                    println("구조적 문제가 있는 토큰: ${e.message}")
+                    false
+                }
+                else -> {
+                    println("검증 중 알수 없는 오류: ${e.message}")
+                    false
+                }
+            }
+        }
+    }
+
+    private fun getClaims(token: String): Claims {
+        val key: Key = SecretKeySpec(secretKey.toByteArray(), "HmacSHA256")
+        val jwtParser: JwtParser = Jwts.parserBuilder().setSigningKey(key).build()
+        return jwtParser.parseClaimsJws(token).body
+    }
+}
