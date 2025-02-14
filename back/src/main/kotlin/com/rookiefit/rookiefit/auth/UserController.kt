1cdf726eae01d52b1714ea2c50d38e29035cf8d8
mjKim <106668821+pipakmj@users.noreply.github.com>
2025-02-15 01:51:37 +0900

    feat: 회원가입 구현

--- back/src/main/kotlin/com/rookiefit/rookiefit/auth/UserController.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/auth/UserController.kt
@@ -1,6 +1,9 @@
 package com.rookiefit.rookiefit.auth
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,5 +14,10 @@ class UserController(private val userService: UserService) {
     @GetMapping("/id-check")
     fun idCheck(@RequestParam userId: String): Boolean {
         return userService.idCheck(userId)
+    }
+    @PostMapping("/sign-up")
+    fun signUp(@RequestBody userDTO: UserDTO): ResponseEntity<String>{
+        userService.signUp(userDTO)
+        return ResponseEntity.ok("회원가입 성공")
     }
 }
--- back/src/main/kotlin/com/rookiefit/rookiefit/auth/UserService.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/auth/UserService.kt
@@ -1,11 +1,23 @@
 package com.rookiefit.rookiefit.auth
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(private val userRepository: UserRepository) {
+    private val passwordEncoder = BCryptPasswordEncoder()
     fun idCheck(userId: String): Boolean {
         val currentUserId = userRepository.findByUserId(userId)
         return currentUserId.isNotEmpty()
+    }
+    fun signUp(userDTO: UserDTO): String {
+        val encryptedPassword = passwordEncoder.encode(userDTO.userPassword)
+        val userEntity = UserEntity(
+            userId = userDTO.userId,
+            userPassword = encryptedPassword,
+            userPhoneNumber = userDTO.userPhoneNumber
+        )
+        userRepository.save(userEntity)
+        return "회원가입 성공 service"
     }
 }
