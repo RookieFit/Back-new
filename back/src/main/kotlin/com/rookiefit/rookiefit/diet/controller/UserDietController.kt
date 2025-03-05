20a0e291d03f6f07c6af7cf914a9d3b890ba5643
kyoungeun <91576339+21270003@users.noreply.github.com>
2025-03-05 23:57:00 +0900

    feat: Jwt사용하여 호출

--- back/src/main/kotlin/com/rookiefit/rookiefit/diet/controller/UserDietController.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/diet/controller/UserDietController.kt
@@ -3,33 +3,37 @@ package com.rookiefit.rookiefit.diet.controller
 import com.rookiefit.rookiefit.diet.dto.UserDietDto
 import com.rookiefit.rookiefit.diet.dto.request.AddFoodToDietRequest
 import com.rookiefit.rookiefit.diet.service.UserDietService
+import com.rookiefit.rookiefit.provider.JwtProvider
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/diet")
-class UserDietController(private val userDietService: UserDietService) {
+class UserDietController(private val userDietService: UserDietService, private val jwtProvider: JwtProvider) {
 
     // 특정 날짜의 식단 조회
     @GetMapping
     fun getUserDiet(
-        @RequestParam userId: String,
+        @RequestHeader("Authorization") authorization: String,
         @RequestParam dietDate: String
     ): ResponseEntity<UserDietDto?> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         val diet = userDietService.getUserDietList(userId, dietDate)
         return ResponseEntity.ok(diet)
     }
 
     // 식단에 음식 추가
     @PostMapping("/list/add")
-    fun addFoodToDiet(@RequestBody request: AddFoodToDietRequest): ResponseEntity<UserDietDto> {
+    fun addFoodToDiet(@RequestBody request: AddFoodToDietRequest, @RequestHeader("Authorization") authorization: String): ResponseEntity<UserDietDto> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         val updatedDiet = userDietService.addFoodToDiet(request)
         return ResponseEntity.ok(updatedDiet)
     }
 
     // 식단에서 특정 음식 삭제
     @DeleteMapping("/list/delete/{detailId}")
-    fun deleteFoodFromDiet(@PathVariable detailId: Long): ResponseEntity<Unit> {
+    fun deleteFoodFromDiet(@PathVariable detailId: Long, @RequestHeader("Authorization") authorization: String): ResponseEntity<Unit> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         userDietService.deleteFoodFromDiet(detailId)
         return ResponseEntity.noContent().build()
     }
@@ -37,10 +41,12 @@ class UserDietController(private val userDietService: UserDietService) {
     // 특정 날짜의 식단 삭제
     @DeleteMapping("/delete")
     fun deleteDietByDate(
-        @RequestParam userId: String,
+        @RequestHeader("Authorization") authorization: String,
         @RequestParam dietDate: String
     ): ResponseEntity<String> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         return userDietService.deleteDietByDate(userId, dietDate)
     }
-
 }
+
+
