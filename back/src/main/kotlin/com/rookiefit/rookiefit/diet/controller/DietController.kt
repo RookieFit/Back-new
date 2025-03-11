f7571d2a0d44076436381220667d79bc69932244
kyoungeun <91576339+21270003@users.noreply.github.com>
2025-03-11 13:54:16 +0900

    chore: token사용 방식으로 수정

--- back/src/main/kotlin/com/rookiefit/rookiefit/diet/controller/DietController.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/diet/controller/DietController.kt
@@ -3,22 +3,34 @@ package com.rookiefit.rookiefit.diet.controller
 import com.rookiefit.rookiefit.diet.dto.FoodInfoDto
 import com.rookiefit.rookiefit.diet.dto.FoodInfoRequestDto
 import com.rookiefit.rookiefit.diet.service.DietService
+import com.rookiefit.rookiefit.provider.JwtProvider
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/diet")
-class DietController(private val dietService: DietService) {
+class DietController(
+    private val dietService: DietService,
+    private val jwtProvider: JwtProvider // JwtProvider 주입
+) {
 
     // 식단 검색
     @GetMapping("/search")
-    fun searchFood(@RequestParam keyword: String): List<FoodInfoDto> {
+    fun searchFood(
+        @RequestParam keyword: String,
+        @RequestHeader("Authorization") authorization: String
+    ): List<FoodInfoDto> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         return dietService.searchFood(keyword)
     }
 
     // 식단 추가
     @PostMapping("/add")
-    fun addFood(@RequestBody foodInfoRequestDto: FoodInfoRequestDto): ResponseEntity<FoodInfoDto> {
+    fun addFood(
+        @RequestBody foodInfoRequestDto: FoodInfoRequestDto,
+        @RequestHeader("Authorization") authorization: String
+    ): ResponseEntity<FoodInfoDto> {
+        val userId = jwtProvider.extractUserId(authorization.replace("Bearer ", ""))
         val savedFood = dietService.addFood(foodInfoRequestDto)
         return ResponseEntity.ok(savedFood)
     }
