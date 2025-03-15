6295390fc089b5d6fe23c0d8018b438e33de33d9
mjKim <106668821+pipakmj@users.noreply.github.com>
2025-03-15 21:33:42 +0900

    feat: 구글맵스를 이용한 주소검색 api 구현

---
+++ back/src/main/kotlin/com/rookiefit/rookiefit/common/googlePlace/GooglePlaceController.kt
@@ -0,0 +1,27 @@
+package com.rookiefit.rookiefit.common.googlePlace
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+
+@RestController
+@RequestMapping("/api/places")
+class GooglePlaceController(private val restTemplate: RestTemplate) {
+    @Value("\${google.places.api.key}")
+    private lateinit var googleApiKey: String
+    private val googlePlacesUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json"
+
+    @GetMapping("/autocomplete")
+    fun autocomplete(@RequestParam query: String?): ResponseEntity<List<String>> {
+        val url = "$googlePlacesUrl?input=$query&key=$googleApiKey&components=country:KR&types=geocode"
+
+        val response = restTemplate.getForObject(url, GooglePlacesResponse::class.java)
+        val predictions = response?.predictions?.map{it.description}?: emptyList()
+        return ResponseEntity.ok(predictions)
+    }
+
+}
---
+++ back/src/main/kotlin/com/rookiefit/rookiefit/common/googlePlace/Prediction.kt
@@ -0,0 +1,5 @@
+package com.rookiefit.rookiefit.common.googlePlace
+
+data class Prediction(
+    val description: String,
+)
