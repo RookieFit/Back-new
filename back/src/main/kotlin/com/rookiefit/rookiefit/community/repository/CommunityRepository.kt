9fed2ee995520c025344e6489c7cc6b8c2cd6aa6
mjKim <106668821+pipakmj@users.noreply.github.com>
2025-03-20 10:54:49 +0900

    chore: 가독성 향상

--- back/src/main/kotlin/com/rookiefit/rookiefit/community/repository/CommunityRepository.kt
+++ back/src/main/kotlin/com/rookiefit/rookiefit/community/repository/CommunityRepository.kt
@@ -6,33 +6,79 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommunityRepository: JpaRepository<CommunityEntity, Long> {
-    fun findByCommunityType(communityType: String, pageable: Pageable): Page<CommunityEntity>
+
+    fun findByCommunityType(
+        communityType: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
     fun findByCommunityId(communityId: Long): CommunityEntity
-    fun findByUserProfile_UserProfileId(userProfileId: Long, pageable: Pageable): Page<CommunityEntity>
+
+    fun findByUserProfile_UserProfileId(
+        userProfileId: Long,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
     fun findByUserProfile_UserProfileIdAndCommunityType(
         userProfileId: Long,
         communityType: String,
         pageable: Pageable
     ): Page<CommunityEntity>
-    fun findByCommunityTitleContaining(query: String, pageable: Pageable): Page<CommunityEntity>
-    fun findByCommunityTitleContainingOrCommunityContentContaining(query1: String, query2: String, pageable: Pageable): Page<CommunityEntity>
-    fun findByCommunityAuthorContaining(query: String, pageable: Pageable): Page<CommunityEntity>
-    fun findByCommunityTypeAndCommunityTitleContaining(communityType: String, query: String, pageable: Pageable): Page<CommunityEntity>
+
+    fun findByCommunityTitleContaining(
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
+    fun findByCommunityTitleContainingOrCommunityContentContaining(
+        query1: String,
+        query2: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
+    fun findByCommunityAuthorContaining(
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
+    fun findByCommunityTypeAndCommunityTitleContaining(
+        communityType: String,
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
     fun findByCommunityTypeAndCommunityTitleContainingOrCommunityContentContaining(
         communityType: String,
         query1: String,
         query2: String,
         pageable: Pageable
     ): Page<CommunityEntity>
-    fun findByCommunityTypeAndCommunityAuthorContaining(communityType: String, query: String, pageable: Pageable): Page<CommunityEntity>
-    fun findByUserProfile_UserProfileIdAndCommunityTitleContaining(userProfileId: Long, query: String, pageable: Pageable): Page<CommunityEntity>
-    fun findByUserProfile_UserProfileIdAndCommunityAuthorContaining(userProfileId: Long, query: String, pageable: Pageable): Page<CommunityEntity>
+
+    fun findByCommunityTypeAndCommunityAuthorContaining(
+        communityType: String,
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
+    fun findByUserProfile_UserProfileIdAndCommunityTitleContaining(
+        userProfileId: Long,
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
+    fun findByUserProfile_UserProfileIdAndCommunityAuthorContaining(
+        userProfileId: Long,
+        query: String,
+        pageable: Pageable
+    ): Page<CommunityEntity>
+
     fun findByUserProfile_UserProfileIdAndCommunityTypeAndCommunityTitleContaining(
         userProfileId: Long,
         communityType: String,
         query: String,
         pageable: Pageable
     ): Page<CommunityEntity>
+
     fun findByUserProfile_UserProfileIdAndCommunityTypeAndCommunityAuthorContaining(
         userProfileId: Long,
         communityType: String,
